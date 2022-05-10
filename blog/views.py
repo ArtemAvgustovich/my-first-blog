22086cb03cadbf9681acfde0470242a9582c66e5
ArtemAvgustovich <artem.avgustovich@gmail.com>
2022-05-10 10:15:01 +0300

    Class-based views instead of functions Only post creator can edit it.

--- blog/views.py
+++ blog/views.py
@@ -1,42 +1,57 @@
-import re
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseForbidden
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic.edit import UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Post
 from .forms import PostForm
 
-# Create your views here.
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+class IndexView(ListView):
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
 
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
 
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+    def get_queryset(self):
+        """
+        Excludes any posts that aren't published yet.
+        """
+        return Post.objects.filter(published_date__lte=timezone.now())
+
+
+class PostNew(LoginRequiredMixin, CreateView):
+    model = Post
+    fields =  ('title', 'text')
+    template_name = 'blog/post_edit.html'
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.published_date = timezone.now()
+        return super().form_valid(form)
+
+
+class PostEdit(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields =  ('title', 'text')
+    template_name_suffix = '_edit'
+    
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        if request.user == post.author:
+            return super().get(request, pk)
+        return HttpResponseForbidden("Access denied! You can't edit other user's posts.")
+
+    def form_valid(self, form):
+        if self.request.user == form.instance.author:
+            form.instance.published_date = timezone.now()
+            return super().form_valid(form)
+        return HttpResponseForbidden("Access denied! You can't edit other user's posts.")
+
